3058a2f097403c784442dc1ca50d3db7bd4e93ba
Malvina Kochkina <malvina@Malvinas-MacBook-Air.local>
2024-07-27 22:46:19 +0200

    add bfs function

--- Program.cs
+++ Program.cs
@@ -10,12 +10,15 @@ namespace Models
         {
             var inputData = "AAA_Y: 0,-5,90\nBCA_C: 10,20,95\nSAC_F: 5,80,65\nARH_B: 100,45,60\nXXX_S: 150,70,180";
             var vessels = ParseInput(inputData);
+            var startVessel = vessels.First();
             var x = CanCommunicate(new Vessel("AAA", 0, -5, 90, 'Y'), new Vessel("BCA", 10, 20, 95, 'C'));
-            //AAA_Y: 0,-5,90
-            //BCA_C: 10,20,95
-            //SAC_F: 5,80,65
-            //ARH_B: 100,45,60
-            //XXX_S: 150,70,180
+            var graph = BuildGraph(vessels);
+            var (pathToShore, distanceToShore) = BfsShortestPath(graph, vessels, startVessel.Id, 'S');
+            var (pathBack, distanceBack) = BfsShortestPath(graph, vessels, pathToShore.Last(), startVessel.Type);
+            var totalDistance = distanceToShore + distanceBack;
+            Console.WriteLine($"Total distance: {totalDistance}");
+            Console.WriteLine("Path to shore: " + string.Join(" -> ", pathToShore));
+            Console.WriteLine("Path back: " + string.Join(" -> ", pathBack));
         }
 
         private static List<Vessel> ParseInput(string inputData)
@@ -38,6 +41,11 @@ namespace Models
             return vessels;
         }
 
+        private static double CalculateDistance(Vessel v1, Vessel v2)
+        {
+            return Math.Sqrt(Math.Pow(v1.X - v2.X, 2) + Math.Pow(v1.Y - v2.Y, 2));
+        }
+
         private static bool CanCommunicate(Vessel v1, Vessel v2)
         {
             var compatibleTypes = new Dictionary<char, char[]>
@@ -48,8 +56,64 @@ namespace Models
                 { 'B', new[] { 'C', 'F', 'B', 'S' } },
                 { 'S', new[] { 'B', 'Y' } }
             };
-            var distance = Math.Sqrt(Math.Pow(v2.X - v1.X, 2) + Math.Pow(v2.Y - v1.Y, 2));
+            //var distance = Math.Sqrt(Math.Pow(v2.X - v1.X, 2) + Math.Pow(v2.Y - v1.Y, 2));
+            var distance = CalculateDistance(v1, v2);
             return compatibleTypes[v1.Type].Contains(v2.Type) && distance <= v1.Range;
+        }
+
+        private static Dictionary<string, List<string>> BuildGraph(List<Vessel> vessels)
+        {
+            var graph = new Dictionary<string, List<string>>();
+            foreach (var vessel in vessels)
+                {
+                graph[vessel.Id] = new List<string>();
+                foreach (var other in vessels)
+                    {
+                    if (vessel.Id != other.Id && CanCommunicate(vessel, other))
+                        {
+                        graph[vessel.Id].Add(other.Id);
+                        }
+                    }
+                }
+
+            return graph;
+        }
+
+        private static (List<string> Path, double Distance) BfsShortestPath(
+            Dictionary<string, List<string>> graph, List<Vessel> vessels, string start, char goalType)
+        {
+            var queue = new Queue<(string, List<string>, double)>();
+            var visited = new HashSet<string>();
+            var vesselDict = vessels.ToDictionary(v => v.Id);
+
+            queue.Enqueue((start, new List<string> { start }, 0.0));
+
+            while (queue.Count > 0)
+                {
+                var (current, path, currentDistance) = queue.Dequeue();
+
+                if (visited.Contains(current))
+                    continue;
+
+                visited.Add(current);
+
+                foreach (var neighbor in graph[current])
+                    {
+                    var newPath = new List<string>(path) { neighbor };
+                    var newDistance = currentDistance + CalculateDistance(vesselDict[current], vesselDict[neighbor]);
+
+                    if (neighbor.Last() == goalType)
+                        {
+                        return (newPath, newDistance);
+                        }
+                    else
+                        {
+                        queue.Enqueue((neighbor, newPath, newDistance));
+                        }
+                    }
+                }
+
+            return (null, 0.0);
         }
     }
 }
